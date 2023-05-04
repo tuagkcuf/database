ddd5d4e78a2012219e4533808c1cf36457921e93
tuagkcuf <99531982+tuagkcuf@users.noreply.github.com>
2023-05-04 09:47:27 +0300

    Add files via upload

---
+++ rgr/database/database/initialization.h
@@ -0,0 +1,9 @@
+#pragma once
+
+using namespace std;
+
+void DInit(Dish *Dishes);
+
+vector<Table> TInit(vector <Table>& Tables);
+
+vector <Worker> WInit(vector <Worker>& Workers);
---
+++ rgr/database/database/models.h
@@ -0,0 +1,23 @@
+#pragma once
+
+struct Dish {
+	int code;
+	string name;
+	string ingredients;
+	int time;
+	int numberOfTable;
+	int calories;
+	Dish *next, *prev;
+};
+
+struct Table {
+	int code;
+	string name;
+	Table *next, *prev;
+};
+
+struct Worker {
+	string secondName, name, phoneNumber, _login, _password;
+	int id, _accessLevel;
+	Worker *next, *prev;
+};
